f052c418c22f71676c828be787cf54851a45bf14
Arcranmon <67716479+Arcranmon@users.noreply.github.com>
2023-04-08 17:49:16 -0400

    PR

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,11 +10,17 @@
       <v-card>
         <v-card-title>About</v-card-title>
         <v-card-text>
-          Welcome to the world of Deskar, the land of Shattered Spirits!
-          <br />
-          <br />General, Move, Strike, and Support icons made by lorc. Defensive icon made by delapouite. Available on https://game-icons.net.
-          <br />
-          <br />
+          Welcome to the world of Deskar, the land of Shattered Spirits!<br />
+          Character art done by Josh McKelvey: https://www.etsy.com/shop/JoshMcKelveyArt<br />
+          Hafted Weapon, Projectile Weapon, Pole Weapon, Improvised Weapon, General, Move, Strike, Unarmed, Support, Land, Stone, and Tremorsight icons made by
+          lorc.<br />
+          Light Armor, Unarmored, Heavy Armor, Sword, Stoneheart, and Grounding icons made by delapouite.<br />
+          Unarmed icon made by skoll.<br />
+          Shield icon made by sbed.<br />
+          Thrown Weapon icon made by Faithtoken.<br />
+          Available on https://game-icons.net.<br /><br />
+          And a big thank you to the playtesters thus far: Aori, everythingElse, Kuji, MesiDoomstalker, and Mystic Witch! Without your feedback and testing none
+          of this would be possible!<br />
           <a href="https://github.com/Arcranmon/shattered-spirits">Click here for more information and the source!</a>
         </v-card-text>
       </v-card>
